4244082279bc73d82c3f3f7eb2b1bb2beb17bcf9
Tailan Patrick <tailanpatrick06@gmail.com>
2025-10-16 18:26:56 -0300

    rodadas as migrations para veículo e seed de dados para criar um administrador

---
+++ Migrations/20251016145456_VeiculosMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api_crud_veiculos_dotnet.Migrations
+{
+    /// <inheritdoc />
+    public partial class VeiculosMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
