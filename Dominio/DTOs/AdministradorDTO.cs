67bc4bd0ea7d878ff938d418e7f63b88cebe0ed8
Tailan Patrick <tailanpatrick06@gmail.com>
2025-10-25 14:47:54 -0300

    criado DTO de administrador

---
+++ Dominio/DTOs/AdministradorDTO.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_crud_veiculos_dotnet.Dominio.DTOs {
+    public class AdministradorDTO {
+        public string Email { get; set; } = default!;
+        public string Senha { get; set; } = default!;
+        public string Perfil { get; set; } = default!;
+
+    }
+}
